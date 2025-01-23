4e0e0c3ea1cf616f6ee2842c3b7d3465da57ec46
904git <guyeongsa@gmail.com>
2025-01-23 12:38:03 +0900

    2025-01-23: 프로그래머스 Lv2 의상 (해결중)

---
+++ Algorithm/src/프로그래머스/Lv2/의상.java
@@ -0,0 +1,89 @@
+package 프로그래머스.Lv2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class 의상 {
+	static String[] selected;
+	static int count;
+
+	public static void main(String[] args) {
+		String[][] clothes = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
+				{ "green_turban", "headgear" }, { "green", "addd" } };
+		System.out.println(solution(clothes));
+
+		// 1 2
+		// 3
+		// 4
+		// 1, 2, 3, 4, 13, 14, 23, 24, 34, 134, 234
+	}
+
+	static public int solution(String[][] clothes) {
+		int answer = 0;
+		HashSet<String> hash = new HashSet<>();
+		for (int i = 0; i < clothes.length; i++) {
+			hash.add(clothes[i][1]);
+		}
+		for (int i = 1; i <= hash.size(); i++) {
+			HashSet<String> type = new HashSet<>(); // 옷 종류
+			select(clothes, type, i, 0, 0);
+		}
+		answer = count;
+		return answer;
+	}
+
+	static public void select(String[][] clothes, HashSet<String> hash, int r, int cur, int cnt) { // 입을 옷 고르기
+		if (cnt == r) {
+			count++;
+			return;
+		}
+
+		for (int i = cur; i < clothes.length; i++) {
+			if (!hash.contains(clothes[i][1])) {
+				hash.add(clothes[i][1]);
+				select(clothes, hash, r, i + 1, cnt + 1);
+				hash.remove(clothes[i][1]);
+			}
+
+		}
+	}
+
+	static public int solution2(String[][] clothes) {
+		int answer = 0;
+		HashMap<String, Integer> typeCnt = new HashMap<>(); // 종류별 개수
+		List<String> types = new ArrayList<>(); // 옷 종류
+		for (int i = 0; i < clothes.length; i++) {
+			if (typeCnt.containsKey(clothes[i][1])) {
+				typeCnt.replace(clothes[i][1], typeCnt.get(clothes[i][1]) + 1);
+			} else {
+				typeCnt.put(clothes[i][1], 1);
+				types.add(clothes[i][1]);
+			}
+		}
+		count = 0;
+		for (int i = 1; i <= types.size(); i++) { // 옷 입는 개수별 경우의 수
+			selected = new String[i];
+			select2(typeCnt, types, i, 0, 0);
+		}
+		answer = count;
+		return answer;
+	}
+
+	static public void select2(HashMap<String, Integer> hash, List<String> list, int r, int cur, int cnt) { // 입을 옷 고르기
+		if (cnt == r) {
+			int result = 1;
+			for (int i = 0; i < selected.length; i++) {
+				result *= hash.get(selected[i]);
+			}
+			count += result;
+			return;
+		}
+
+		for (int i = cur; i < list.size(); i++) {
+			selected[cnt] = list.get(i);
+			select2(hash, list, r, i + 1, cnt + 1);
+		}
+	}
+}
