8e9357432c6f60338df2f83c54dbac9327ef1405
904git <guyeongsa@gmail.com>
2025-05-28 21:18:12 +0900

    2025-05-28: 프로그래머스 LV2 혼자서 하는 틱택토(수정 필요)

---
+++ Algorithm/src/프로그래머스/Lv2/혼자서하는틱택토.java
@@ -0,0 +1,64 @@
+package 프로그래머스.Lv2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 혼자서하는틱택토 {
+	static int[] di = { -1, -1, 0, 1, 1, 1, 0, -1 }, dj = { 0, 1, 1, 1, 0, -1, -1, -1 }; // 8방향
+
+	public static void main(String[] args) {
+		String[] board = { "XXX", "XOO", "OOO" };
+		System.out.println(solution(board));
+	}
+
+	static public int solution(String[] board) { // 수정 필요
+		int[] cnt = { 0, 0 }; // O의 개수, X의 개수
+		Queue<int[]> queue = new LinkedList<>(); // 줄이 완성되었는지 확인
+		boolean[][] visit = new boolean[3][3]; // 방문 기록
+		boolean flag = false; // 한 줄이 완성되었는지
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (board[i].charAt(j) != '.') {
+					if (board[i].charAt(j) == 'O') {
+						cnt[0] += 1;
+					} else {
+						cnt[1] += 1;
+					}
+
+					if (!flag) {
+						queue.add(new int[] { i, j });
+						visit[i][j] = true;
+						int t = 0;
+						while (!queue.isEmpty()) {
+							int size = queue.size();
+							for (int s = 0; s < size; s++) {
+								int[] p = queue.poll();
+
+								for (int d = 0; d < di.length; d++) {
+									int ni = p[0] + di[d];
+									int nj = p[1] + dj[d];
+
+									if (0 <= ni && ni < 3 && 0 <= nj && nj < 3 && !visit[ni][nj]
+											&& board[ni].charAt(nj) == board[i].charAt(j)) {
+										// 범위 안에 있으며 아직 방문하지 않고 같은 문자일 때 bfs
+										queue.add(new int[] { ni, nj });
+										visit[ni][nj] = true;
+									}
+								}
+							}
+							t += 1;
+						}
+						if (t == 3) { // 한 줄이 완성됨
+							flag = true;
+						}
+					}
+				}
+			}
+		}
+		if ((cnt[0] != cnt[1] && cnt[1] + 1 != cnt[0]) || (flag && cnt[0] + cnt[1] < 9)) {
+			// 순서가 잘못되거나 (O의 개수가 X보다 1개 크거나 같아야 함), 한 줄이 완성되었는데 게임이 안 끝났을 때
+			return 0;
+		}
+		return 1;
+	}
+}
